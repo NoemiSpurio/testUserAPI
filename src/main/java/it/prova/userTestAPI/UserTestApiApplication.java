bd249728fd197b0c832b4385e5b833a7ec7b40bf
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-21 09:24:41 +0100

    creazione model

---
+++ src/main/java/it/prova/userTestAPI/UserTestApiApplication.java
@@ -0,0 +1,13 @@
+package it.prova.userTestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTestApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/it/prova/userTestAPI/model/Utente.java
@@ -0,0 +1,108 @@
+package it.prova.userTestAPI.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "utente")
+public class Utente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "dateCreated")
+	private LocalDate dateCreated;
+	@Column(name = "token")
+	private String token;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ruolo_id", nullable = false)
+	private Ruolo ruolo;
+
+	public Utente() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public LocalDate getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(LocalDate dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Ruolo getRuolo() {
+		return ruolo;
+	}
+
+	public void setRuolo(Ruolo ruolo) {
+		this.ruolo = ruolo;
+	}
+
+}
